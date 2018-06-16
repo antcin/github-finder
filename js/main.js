761faa9ac175ae1581b6b8a6a3c3af3d2940d49e
antcin <ciniglioantonio@gmail.com>
2018-06-16 11:24:50 +0100

    finalise app; issues with bootstrap, though; not looking pretty

--- js/main.js
+++ js/main.js
@@ -13,6 +13,38 @@ $(document).ready(function(){
         client_secret: client_secret
       }
     }).done(function(user){
+      $.ajax({
+        url: `https://api.github.com/users/${username}/repos`,
+        data: {
+          client_id: client_id,
+          client_secret: client_secret,
+          sort: 'created: asc',
+          per_page: 5
+        }
+      }).done(function(repos) {
+        $.each(repos, function(index, repo){
+          $('#repos').append(`
+            <div class="well">
+              <div class="row">
+                <div class="col-md-7">
+                  <strong>${repo.name}</strong>: ${repo.description}
+                </div>
+
+                <div class="col-md-3">
+                  <span class="label label-default">Forks: ${repo.forks_count}</span>
+                  <span class="label label-primary">Watchers: ${repo.watchers_count}</span>
+                  <span class="label label-success">Stars: ${repo.stargazers_count}</span>
+                </div>
+
+                <div class="col-md-2">
+                  <a href="${repo.html_url}" target="_blank" class="btn btn-default">Repo page</a>
+                </div>
+
+              </div>
+            </div>
+          `);
+        })
+      })
       $('#profile').html(`
         <div class="panel panel-default">
           <div class="panel-heading">
@@ -40,6 +72,8 @@ $(document).ready(function(){
             </div>
           </div>
         </div>
+        <h3 class="page-header">Latest Repos</h3>
+        <div id="repos"></div>
       `)
     });
   });
