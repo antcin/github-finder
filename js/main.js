29155443b39fb77b6c13933366f45d5daf1d06b5
antcin <ciniglioantonio@gmail.com>
2018-06-16 11:15:12 +0100

    display more user info

--- js/main.js
+++ js/main.js
@@ -21,9 +21,21 @@ $(document).ready(function(){
           <div class="panel-body">
             <div class="row">
               <div class="col-md-3">
-                <img src="${user.avatar_url}" class="thumbnail" />
+                <img src="${user.avatar_url}" class="thumbnail avatar" />
+                <a target="_blank" class="btn btn-primary btn-block" href="${user.html_url}">View profile</a>
               </div>
               <div class="col-md-9">
+                <span class="label label-default">Public Repos: ${user.public_repos}</span>
+                <span class="label label-primary">Public Gists: ${user.public_gists}</span>
+                <span class="label label-success">Followers: ${user.followers}</span>
+                <span class="label label-info">Following: ${user.following}</span>
+                <br><br>
+                <ul class="list-group">
+                  <li class="list-group-item">Company: ${user.company}</li>
+                  <li class="list-group-item">Website/Blog: ${user.blog}</li>
+                  <li class="list-group-item">Location: ${user.location}</li>
+                  <li class="list-group-item">Member since: ${user.created_at}</li>
+                </ul>
               </div>
             </div>
           </div>
